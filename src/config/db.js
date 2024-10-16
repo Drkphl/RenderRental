2a38ccc2a8b3e7b7f35adbf7e4d95f88007aee59
Drkphl <leonardotizon@hotmail.com>
2024-10-16 20:01:33 -0300

    ultima atualizacao

--- src/config/db.js
+++ src/config/db.js
@@ -1,14 +1,12 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
-const connectDB = async() => {
+const connectDB = async () => {
     try {
         await mongoose.connect(
             `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.CLUSTER_ADDRESS}/${process.env.DB_NAME}`
-          );
-
+        );
     } catch (error) {
         console.error('Erro ao conectar ao MongoDB', error);
-
     }
 };
 
