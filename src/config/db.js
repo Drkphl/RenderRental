dfb251116c6d21ba0252cdc491d4f53bf711e2f7
Drkphl <leonardotizon@hotmail.com>
2024-10-16 21:05:56 -0300

    corrigido colecao

--- src/config/db.js
+++ src/config/db.js
@@ -4,7 +4,7 @@ const connectDB = async () => {
     try {
         await mongoose.connect(
             //`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.CLUSTER_ADDRESS}/${process.env.DB_NAME}`
-            `mongodb://localhost:27017`
+            `mongodb://localhost:27017/Locadora`
         );
     } catch (error) {
         console.error('Erro ao conectar ao MongoDB', error);
