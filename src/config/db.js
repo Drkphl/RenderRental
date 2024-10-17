fa60d61a7376b0c6ff54689f0afb66c3b715aeb4
Drkphl <leonardotizon@hotmail.com>
2024-10-16 21:08:59 -0300

    caralha mesmo

--- src/config/db.js
+++ src/config/db.js
@@ -3,8 +3,8 @@ import mongoose from 'mongoose';
 const connectDB = async () => {
     try {
         await mongoose.connect(
-            //`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.CLUSTER_ADDRESS}/${process.env.DB_NAME}`
-            `mongodb://localhost:27017/Locadora`
+            `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.CLUSTER_ADDRESS}/${process.env.DB_NAME}`
+            //`mongodb://localhost:27017/Locadora`
         );
     } catch (error) {
         console.error('Erro ao conectar ao MongoDB', error);
